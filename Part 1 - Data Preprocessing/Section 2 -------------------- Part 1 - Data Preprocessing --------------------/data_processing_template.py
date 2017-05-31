6f8e90b8d7c29c9c8ecb5640f9279e7784e3a459
Eduardo Luz <luz.eduardo@gmail.com>
2017-05-31 00:56:01 -0300

    feature scaling

--- Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_processing_template.py
+++ Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_processing_template.py
@@ -40,4 +40,12 @@ from sklearn.model_selection import train_test_split
 #defining 20% of data to test dataset
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
+
+##Featuring scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+
 print X
+
