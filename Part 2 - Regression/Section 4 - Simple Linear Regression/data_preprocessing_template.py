91b638cf54da0b9ac256fdd41adb5d575f4582e7
Eduardo Luz <luz.eduardo@gmail.com>
2017-06-10 20:09:51 -0300

    salary linear regression

--- Part 2 - Regression/Section 4 - Simple Linear Regression/data_preprocessing_template.py
+++ Part 2 - Regression/Section 4 - Simple Linear Regression/data_preprocessing_template.py
@@ -1,8 +1,6 @@
 # Data Preprocessing Template
 
 # Importing the libraries
-import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
@@ -11,8 +9,8 @@ X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 1].values
 
 # Splitting the dataset into the Training set and Test set
-from sklearn.cross_validation import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state = 0)
 
 # Feature Scaling
 """from sklearn.preprocessing import StandardScaler
@@ -21,3 +19,12 @@ X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
 sc_y = StandardScaler()
 y_train = sc_y.fit_transform(y_train)"""
+
+#fit linear regression  to the training set
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+y_pred = regressor.predict(X_test)
+
+
+stop = 1
